df815f39a8616d6d14df5ad2748331b41c6074f2
Tickbase <iver.kleivbakk@gmail.com>
2025-05-18 11:55:17 +0200

    fix errors

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -43,7 +43,7 @@ export function truncateString(str: string, maxLength: number, suffix: string = 
  * @param delay Delay in milliseconds
  * @returns Debounced function
  */
-export function debounce<T extends (...args: any[]) => any>(
+export function debounce<T extends (...args: unknown[]) => unknown>(
   fn: T,
   delay: number
 ): (...args: Parameters<T>) => void {
@@ -66,7 +66,7 @@ export function debounce<T extends (...args: any[]) => any>(
  * @param limit Interval in milliseconds
  * @returns Throttled function
  */
-export function throttle<T extends (...args: any[]) => any>(
+export function throttle<T extends (...args: unknown[]) => unknown>(
   fn: T,
   limit: number
 ): (...args: Parameters<T>) => void {
