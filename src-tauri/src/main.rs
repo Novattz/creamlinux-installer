a2d5a38f68b91758941e43151eb387dab7f7c0d8
Novattz <iver.kleivbakk@gmail.com>
2025-09-10 03:01:17 +0200

    bruh x2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -513,7 +513,7 @@ fn main() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        // .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
