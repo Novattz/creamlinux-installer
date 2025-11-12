0a5f00d3fbd3bc80c120ccab11c2c97d820e0b83
Novattz <iver.kleivbakk@gmail.com>
2025-11-12 15:02:13 +0100

    Remove redundant files

--- src/components/updater/index.ts
+++
@@ -1,5 +0,0 @@
-// Update checker implementation
-export { default as useUpdateChecker } from '@/hooks/useUpdateChecker'
-
-// Simple component for using the checker
-export { default as UpdateNotifier } from './UpdateNotifier'
