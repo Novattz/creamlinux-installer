3459158d3fb42907eae325514f23c0866cf2d1af
Novattz <iver.kleivbakk@gmail.com>
2026-01-03 00:24:56 +0100

    config types #88

---
+++ src/types/Config.ts
@@ -0,0 +1,8 @@
+/**
+ * User configuration structure
+ * Matches the Rust Config struct
+ */
+export interface Config {
+  /** Whether to show the disclaimer on startup */
+  show_disclaimer: boolean
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './Game'
 export * from './DlcInfo'
+export * from './Config'
